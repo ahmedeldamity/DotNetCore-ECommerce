02891e71dfa2199dde6e57e54fa0040c084f5599
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-03-07 11:22:36 +0200

    Product service

---
+++ Core/Interfaces/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Product_Entities;
+using Core.Specifications.ProductSpecifications;
+
+namespace Core.Interfaces.Services
+{
+    public interface IProductService
+    {
+        Task<IReadOnlyList<Product>> GetProductsAsync(ProductSpecificationParameters specParams);
+        Task<Product?> GetProductAsync(int id);
+    }
+}
---
+++ Core/Specifications/ProductSpecifications/ProductWithBrandAndCategorySpecifications.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities.Product_Entities;
+
+namespace Core.Specifications.ProductSpecifications
+{
+    public class ProductWithBrandAndCategorySpecifications: BaseSpecification<Product>
+    {
+        public ProductWithBrandAndCategorySpecifications(ProductSpecificationParameters specParams)
+        {
+            IncludesCriteria.Add(p => p.Brand);
+            IncludesCriteria.Add(p => p.Category);
+            WhereCriteria =
+               p => (string.IsNullOrEmpty(specParams.search) || p.Name.ToLower().Contains(specParams.search.ToLower())) &&
+               (!specParams.brandId.HasValue || p.BrandId == specParams.brandId.Value) &&
+               (!specParams.categoryId.HasValue || p.CategoryId == specParams.categoryId.Value);
+        }
+        public ProductWithBrandAndCategorySpecifications(int id)
+        {
+            WhereCriteria = p => p.Id == id;
+            IncludesCriteria.Add(p => p.Brand);
+            IncludesCriteria.Add(p => p.Category);
+        }
+    }
+}
